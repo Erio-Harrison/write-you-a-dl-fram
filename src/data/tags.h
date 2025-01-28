c5c5aa44fde17ee3bf74c80f60b8bf0e793ab2ac
Erio-Harrison <u7541840@gmail.com>
2025-01-28 18:15:31 +1100

    implement data's tages and traits

---
+++ src/data/tags.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/// data types
+struct CategoryTags
+{
+    struct Scalar;
+    struct Matrix;
+    struct BatchScalar;
+    struct BatchMatrix;
+};
+
+/// device types
+struct DeviceTags
+{
+    struct CPU;
+};
