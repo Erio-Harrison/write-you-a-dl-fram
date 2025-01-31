ed6f2e490aae74570ff0bf5a4495bd0befbdb35d
Erio-Harrison <u7541840@gmail.com>
2025-01-31 22:02:48 +1100

    add GPU's scalar but leave notes

--- src/data/scalar.h
+++ src/data/scalar.h
@@ -51,3 +51,67 @@ public:
 private:
     ElementType m_elem;
 };
+
+// template <typename TElem, typename TDevice = DeviceTags::GPU>
+// class Scalar
+// {
+//     static_assert(std::is_same<RemConstRef<TElem>, TElem>::value);
+
+// public:
+//     using ElementType = TElem;
+//     using DeviceType = TDevice;
+
+// public:
+//     Scalar(ElementType elem = ElementType())
+//         : m_elem(elem)
+//     {
+//         m_gpuElem = Allocator<DeviceTags::GPU>::Allocate<ElementType>(1);
+//         cudaMemcpy(m_gpuElem, &m_elem, sizeof(ElementType), cudaMemcpyHostToDevice);
+//     }
+
+//     ~Scalar()
+//     {
+//         // No need to manually free the memory since shared_ptr handles it
+//     }
+
+//     auto& Value()
+//     {
+//         cudaMemcpy(&m_elem, m_gpuElem, sizeof(ElementType), cudaMemcpyDeviceToHost);
+//         return m_elem;
+//     }
+
+//     auto Value() const
+//     {
+//         ElementType temp;
+//         cudaMemcpy(&temp, m_gpuElem, sizeof(ElementType), cudaMemcpyDeviceToHost);
+//         return temp;
+//     }
+
+//     bool operator==(const Scalar& val) const
+//     {
+//         ElementType otherElem;
+//         cudaMemcpy(&otherElem, val.m_gpuElem, sizeof(ElementType), cudaMemcpyDeviceToHost);
+//         return m_elem == otherElem;
+//     }
+
+//     template <typename TOtherType>
+//     bool operator==(const TOtherType&) const
+//     {
+//         return false;
+//     }
+
+//     template <typename TData>
+//     bool operator!=(const TData& val) const
+//     {
+//         return !(operator==(val));
+//     }
+
+//     auto EvalRegister() const
+//     {
+//         return MakeConstEvalHandle(*this);
+//     }
+
+// private:
+//     ElementType m_elem;
+//     ElementType* m_gpuElem = nullptr;
+// };
